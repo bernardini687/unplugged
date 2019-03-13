3f93da9cbaac333bc667ac457ea48c07827eb145
Oscar Bernardini <bernardini687@gmail.com>
2019-03-13 21:41:15 +0100

    Init app with Member model from 'devise'

---
+++ app/models/member.rb
@@ -0,0 +1,5 @@
+class Member < ApplicationRecord
+  # :confirmable, :lockable, :timeoutable, :trackable, :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+end
