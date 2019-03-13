bc4e58fe6aec1c8058be39ee88372b5fd774e60e
Oscar Bernardini <bernardini687@gmail.com>
2019-03-13 23:38:25 +0100

    Add Instrument model and index, show and new views

---
+++ app/controllers/instruments_controller.rb
@@ -0,0 +1,55 @@
+class InstrumentsController < ApplicationController
+  skip_before_action :authenticate_member!, only: %i[index show]
+  before_action :set_instrument, only: %i[show edit update]
+
+  def index
+    if params[:query].blank?
+      @instruments = Instrument.all
+    else
+      # Why the % around #{}?
+      @instruments = Instrument.where('category LIKE ?', "%#{params[:query]}%")
+      # @categories = Instrument.all.map { |i| i.category.capitalize }
+    end
+  end
+
+  def show; end
+
+  def new
+    @member = current_member
+    @instrument = Instrument.new
+    @categories = ['Keyboard', 'Guitar', 'Bass', 'Wind instrument', 'Brass instrument', 'Bowed instrument']
+    @attributes = ['awesome', 'graceful', 'epic', 'good enough']
+    @prices = (5..100).step(5).to_a
+  end
+
+  def edit; end
+
+  def create
+    @instrument = Instrument.new(instrument_params)
+    @instrument.member = current_member
+
+    if @instrument.save
+      redirect_to @instrument
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    if @instrument.update(instrument_params)
+      redirect_to @instrument
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def set_instrument
+    @instrument = Instrument.find(params[:id])
+  end
+
+  def instrument_params
+    params.require(:instrument).permit(:category, :address, :description, :photo, :price)
+  end
+end
