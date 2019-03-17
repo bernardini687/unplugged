04ab009bd558c2017de15e401743180f1fb6e0b6
Oscar Bernardini <bernardini687@gmail.com>
2019-03-17 23:27:07 +0100

    Hard work on making stimulus show instrument details

--- app/javascript/controllers/instruments_controller.js
+++ app/javascript/controllers/instruments_controller.js
@@ -1,9 +1,16 @@
 import { Controller } from "stimulus";
 
 export default class extends Controller {
-  static targets = [ 'list' ];
+  static targets = [ 'list', 'details' ];
+
+  show(e) {
+    e.preventDefault();
+    // console.log(e.originalTarget.dataset.index);
+    this.detailsTargets.find(instrument => instrument.dataset.index === e.originalTarget.dataset.index).classList.toggle('hidden');
+  }
 
   reveal() {
+    // console.log('reveal')
     this.listTarget.classList.toggle('hidden');
   }
 }
