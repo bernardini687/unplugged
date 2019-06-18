0a1d883e0d727b5be8810d1a260368c0874b9f12
Oscar Bernardini <bernardini687@gmail.com>
2019-06-18 07:59:03 +0200

    Add columns to Bookings

--- db/migrate/20190618054808_create_bookings.rb
+++ db/migrate/20190618054808_create_bookings.rb
@@ -2,6 +2,10 @@ class CreateBookings < ActiveRecord::Migration[5.2]
   def change
     create_table :bookings do |t|
       t.references :user, foreign_key: true
+      t.references :instrument, foreign_key: true
+      t.date :checkin
+      t.date :checkout
+      t.boolean :confirmed
 
       t.timestamps
     end
