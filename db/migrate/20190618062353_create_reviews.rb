3581b18fbd6125108e6a6a066891b96934950a7f
Oscar Bernardini <bernardini687@gmail.com>
2019-06-18 08:28:53 +0200

    Add precision and scale to reviews' rating

--- db/migrate/20190618062353_create_reviews.rb
+++ db/migrate/20190618062353_create_reviews.rb
@@ -3,7 +3,7 @@ class CreateReviews < ActiveRecord::Migration[5.2]
     create_table :reviews do |t|
       t.references :booking, foreign_key: true
       t.text :content
-      t.decimal :rating
+      t.decimal :rating, precision: 2, scale: 1
 
       t.timestamps
     end
