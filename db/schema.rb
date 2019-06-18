6728b12a884f15742e3fe309dfb4d416774e7802
Oscar Bernardini <bernardini687@gmail.com>
2019-06-18 08:19:55 +0200

    Reset database and migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_17_075126) do
+ActiveRecord::Schema.define(version: 2019_06_18_054808) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "bookings", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "instrument_id"
+    t.date "checkin"
+    t.date "checkout"
+    t.boolean "confirmed"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["instrument_id"], name: "index_bookings_on_instrument_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
   create_table "instruments", force: :cascade do |t|
     t.string "brand"
     t.string "category"
-    t.decimal "price"
+    t.decimal "price", precision: 10, scale: 2
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
@@ -37,5 +49,7 @@ ActiveRecord::Schema.define(version: 2019_06_17_075126) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "instruments"
+  add_foreign_key "bookings", "users"
   add_foreign_key "instruments", "users", name: "owner", on_delete: :cascade
 end
