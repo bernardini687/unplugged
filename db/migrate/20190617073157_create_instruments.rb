9860b07ccfdfcb26c851868a5c91e5281a993a22
Oscar Bernardini <bernardini687@gmail.com>
2019-06-18 08:01:43 +0200

    Add precision and scale to price column on instruments

--- db/migrate/20190617073157_create_instruments.rb
+++ db/migrate/20190617073157_create_instruments.rb
@@ -3,7 +3,7 @@ class CreateInstruments < ActiveRecord::Migration[5.2]
     create_table :instruments do |t|
       t.string :brand
       t.string :category
-      t.decimal :price
+      t.decimal :price, precision: 10, scale: 2
 
       t.timestamps
     end
