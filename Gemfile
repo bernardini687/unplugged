725fd45d9f86513e9f26a43a87be45654ab0a4fe
Oscar Bernardini <bernardini687@gmail.com>
2019-06-19 09:33:16 +0200

    Update capybara gem and code instruments#create

--- Gemfile
+++ Gemfile
@@ -32,5 +32,5 @@ group :development, :test do
 end
 
 group :test do
-  gem 'capybara', '~> 2.3.0'
+  gem 'capybara', '~> 2.7.1'
 end
--- app/controllers/instruments_controller.rb
+++ app/controllers/instruments_controller.rb
@@ -1,11 +1,32 @@
 class InstrumentsController < ApplicationController
   skip_before_action :authenticate_user! # , only: %i[index]
+  # before_action :set_instrument, only: %i[show edit update destroy]
 
   def index
+    @instruments = Instrument.all
   end
 
   def new
     @instrument = Instrument.new
     @categories = Instrument.categories
+  end
+
+  def create
+    @instrument = Instrument.new(instrument_params)
+    if @instrument.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_instrument
+    @instrument = Instrument.find(params[:id])
+  end
+
+  def instrument_params
+    params.require(:instrument).permit(:brand, :model, :category)
   end
 end
