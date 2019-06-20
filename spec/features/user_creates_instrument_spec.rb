0934343bce3425a6ffe0f690c741e18d12285692
Oscar Bernardini <bernardini687@gmail.com>
2019-06-20 08:51:49 +0200

    Temporarily disable devise for instruments#new and #create

--- spec/features/user_creates_instrument_spec.rb
+++ spec/features/user_creates_instrument_spec.rb
@@ -6,18 +6,18 @@ feature 'User creates instrument' do
 
     click_on 'Add your instrument'
 
-    click_on 'Sign up'
-    fill_in 'Email', with: 'test@mail.com'
-    fill_in 'Password', with: '123456', exact: true
-    fill_in 'Password confirmation', with: '123456', exact: true
-    binding.pry
-    page.execute_script("$('form#new-user').submit()")
+    # click_on 'Sign up'
+    # fill_in 'Email', with: 'test@mail.com'
+    # fill_in 'Password', with: '123456', exact: true
+    # fill_in 'Password confirmation', with: '123456', exact: true
+    # binding.pry
+    # page.execute_script("$('form#new-user').submit()")
 
     fill_in 'Brand', with: 'Fender'
     fill_in 'Model', with: 'Stratocaster'
     select 'Guitar', from: 'Category'
     click_on 'Submit'
 
-    expect(page).to have_css '.instrument h3', text: 'Fender Stratocaster'
+    expect(page).to have_css '.instruments h4', text: 'Fender Stratocaster'
   end
 end
