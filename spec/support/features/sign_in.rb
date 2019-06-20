58c13417d62ecd890e33987fc2a254c9b8594d85
Oscar Bernardini <bernardini687@gmail.com>
2019-06-20 09:59:42 +0200

    Remove password confirmation field

--- spec/support/features/sign_in.rb
+++ spec/support/features/sign_in.rb
@@ -4,8 +4,8 @@ module Features
 
     click_on 'Sign up / Sign in'
     fill_in 'Email', with: 'test1@mail.com'
-    fill_in 'Password', with: '123456', exact: true
-    fill_in 'Password confirmation', with: '123456', exact: true
-    page.execute_script("$('form#new-user').submit()")
+    fill_in 'Password', with: '123456'
+    click_on 'Sign up'
+    save_and_open_page
   end
 end
