e4de372fca8500c2ba6310a8d48b3bb9dc1054da
Oscar Bernardini <bernardini687@gmail.com>
2019-06-22 13:00:15 +0200

    Add edit routes, form and link

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'instruments#index'
 
-  resources :instruments, only: %i[new create]
+  resources :instruments, only: %i[new create edit update]
   devise_for :users
 end
