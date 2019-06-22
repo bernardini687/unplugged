2ac0f2f6bb4c0f45391dfaab17d6445c035ebd42
Oscar Bernardini <bernardini687@gmail.com>
2019-06-22 12:56:42 +0200

    Create test for intrument updates

---
+++ spec/features/user_updates_intrument_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+feature 'User updates instrument' do
+  scenario 'successfully' do
+    sign_in
+    create_instrument(model: 'Telecaster')
+    expect(page).to have_css '.instruments h4', text: 'Fender Telecaster'
+
+    update_instrument(model: 'Stratocaster')
+    expect(page).to have_css '.instruments h4', text: 'Fender Stratocaster'
+  end
+end
