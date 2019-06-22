c042436c81367916f586750e68e6bda6419ae83f
Oscar Bernardini <bernardini687@gmail.com>
2019-06-22 13:38:27 +0200

    Add show routes, view and instruments#show

--- app/controllers/instruments_controller.rb
+++ app/controllers/instruments_controller.rb
@@ -1,23 +1,35 @@
 class InstrumentsController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[index]
-  # before_action :set_instrument, only: %i[show edit update destroy]
+  before_action :set_instrument, only: %i[show edit update destroy]
+  before_action :set_categories, only: %i[new edit]
 
   def index
     @instruments = Instrument.all
   end
 
+  def show; end
+
   def new
     @instrument = Instrument.new
-    @categories = Instrument.categories
   end
+
+  def edit; end
 
   def create
     @instrument = Instrument.new(instrument_params)
     @instrument.user = current_user
     if @instrument.save
-      redirect_to root_path
+      redirect_to root_url
     else
       render :new
+    end
+  end
+
+  def update
+    if @instrument.update_attributes(instrument_params)
+      redirect_to @instrument
+    else
+      render :edit
     end
   end
 
@@ -25,6 +37,10 @@ class InstrumentsController < ApplicationController
 
   def set_instrument
     @instrument = Instrument.find(params[:id])
+  end
+
+  def set_categories
+    @categories = Instrument.categories
   end
 
   def instrument_params
