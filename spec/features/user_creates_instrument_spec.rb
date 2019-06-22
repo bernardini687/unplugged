f97327b187dc2e28edb731d55dd01ec28e85a3f0
Oscar Bernardini <bernardini687@gmail.com>
2019-06-22 12:59:24 +0200

    Refactor instrument create test

--- spec/features/user_creates_instrument_spec.rb
+++ spec/features/user_creates_instrument_spec.rb
@@ -3,14 +3,7 @@ require 'rails_helper'
 feature 'User creates instrument' do
   scenario 'successfully' do
     sign_in
-
-    click_on 'Add your instrument'
-
-    fill_in 'Brand', with: 'Fender'
-    fill_in 'Model', with: 'Stratocaster'
-    select 'Guitar', from: 'Category'
-    click_on 'Submit'
-
-    expect(page).to have_css '.instruments h4', text: 'Fender Stratocaster'
+    create_instrument
+    expect(page).to have_css '.instruments h4', text: 'Fender Jaguar'
   end
 end
